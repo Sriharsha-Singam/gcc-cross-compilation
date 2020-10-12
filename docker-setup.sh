67195dfa319026213eee502d703b7d6670840094
Harsh <harshasingam3@gmail.com>
2020-10-12 14:53:47 -0400

    adding-sudo

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,5 @@
-apt-get install docker -y
-docker pull centos:centos7
+sudo apt-get install docker -y
+sudo docker pull centos:centos7
 current_dir=$(pwd)
 sudo docker run --rm --privileged --name centos7 -v $current_dir:/src -w /src centos:centos7 ./gcc_install.sh
 tar -czf gcc_centos_x86_64.tar.gz custom
