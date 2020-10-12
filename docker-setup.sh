e81ec478e5382413a19c047e715b8115aa0bc0d5
Harsh <harshasingam3@gmail.com>
2020-10-12 14:50:10 -0400

    fix-bash

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,5 @@
-yum install docker -y
+apt-get install docker -y
 docker pull centos:centos7
 current_dir=$(pwd)
-docker run -ti -v $current_dir:/src --name centos7 centos:centos7 /bin/bash
 sudo docker run --rm --privileged --name centos7 -v $current_dir:/src -w /src centos:centos7 ./gcc_install.sh
+tar -czf gcc_centos_x86_64.tar.gz custom
